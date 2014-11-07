b3c8424864e7e97f45e7768165c634609f5d42db
pavel <kalashnikovisme@gmail.com>
2014-11-08 02:00:21 +0300

    add slides

--- layout.js
+++ layout.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
   $('.current').css('margin-top', $(this).height() - 220 - $('.current').height());
+  $('#show_submenu_1').click(function() {
+    $('#submenu_1').animate({
+      width: 'toggle'
+    }, 350);
+    $('.submenu-navbar').each(function() {
+      if (this.id != 'submenu_1') {
+        if ($(this).css('display') == 'block') {
+          $(this).hide();
+        }
+      }
+    });
+  });
+  $('#submenu_1 span').click(function() {
+    $('.submenu-navbar').each(function() {
+      if (this.id != 'submenu_1') {
+        if ($(this).css('display') == 'block') {
+          $(this).hide();
+        }
+      }
+    });
+    $('#submenu_' + this.id).animate({
+      width: 'toggle'
+    }, 350);
+  });
 });
